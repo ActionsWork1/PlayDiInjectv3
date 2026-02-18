3097214b3d8cf455c6bfe5ad3294e21849e5d6de
alphaawana <alpha.gurjar01@gmail.com>
2026-02-19 02:17:46 +0530

    run #5

--- src/test/java/auto01/uitests2/LoginTest2.java
+++ src/test/java/auto01/uitests2/LoginTest2.java
@@ -9,6 +9,7 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
+import testutil.TestHelper;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -26,14 +27,14 @@ public class LoginTest2 extends BaseUiTest2 {
     @Test(priority = 1,groups = {"ui", "smoke","grp2"})
     public void loginSmokeTest() {
         // Print the result
-        System.out.println("Current Date Time: " + currentTimeStamp());
+        System.out.println("Current Date Time: " + TestHelper.currentTimeStamp());
         loginPage.NavigateTo("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         loginPage.HRMLogin("Admin", "admin123");
     }
 
     @Test(priority = 2, groups = {"ui","grp2"})
     public void EmployeePageTest() {
-        System.out.println("Current Date Time: " + currentTimeStamp());
+        System.out.println("Current Date Time: " + TestHelper.currentTimeStamp());
         page.getByRole(AriaRole.LINK,new Page.GetByRoleOptions().setName("PIM")).click();
         try { Thread.sleep(5000); }
         catch (InterruptedException e) { throw new RuntimeException(e); }
@@ -41,29 +42,11 @@ public class LoginTest2 extends BaseUiTest2 {
 
     @Test(priority = 3, groups = {"ui","grp2"})
     public void AdminPageTest() {
-        System.out.println("Current Date Time: " + currentTimeStamp());
+        System.out.println("Current Date Time: " + TestHelper.currentTimeStamp());
         page.getByRole(AriaRole.LINK,new Page.GetByRoleOptions().setName("Admin")).click();
         try { Thread.sleep(5000); }
         catch (InterruptedException e) { throw new RuntimeException(e); }
     }
-
-
-    public String currentTimeStamp(){
-        // Get the current date and time
-        LocalDateTime currentDateTime = LocalDateTime.now();
-
-        // Define the desired format pattern with "hh" for 12-hour format and "a" for AM/PM marker
-        // Using Locale.ENGLISH ensures the "AM/PM" string is consistently English
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a", Locale.ENGLISH);
-
-        // Format the current date and time
-        String formattedDateTime = currentDateTime.format(formatter);
-        return formattedDateTime;
-
-    }
-
-
-
 
 }
 
--- src/test/java/testutil/TestHelper.java
+++ src/test/java/testutil/TestHelper.java
@@ -1,4 +1,22 @@
 package testutil;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class TestHelper {
+
+    public static String currentTimeStamp(){
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Define the desired format pattern with "hh" for 12-hour format and "a" for AM/PM marker
+        // Using Locale.ENGLISH ensures the "AM/PM" string is consistently English
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a", Locale.ENGLISH);
+
+        // Format the current date and time
+        String formattedDateTime = currentDateTime.format(formatter);
+        return formattedDateTime;
+
+    }
 }
